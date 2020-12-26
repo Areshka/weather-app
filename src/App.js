adc31454812e5b8d0ec3982417eb4c97afe6acd4
Areshka <oleg1990moskalenko@gmail.com>
2020-12-27 01:29:17 +0200

    added Aplication

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Layout from "./containers/Layout";
+import Application from "./containers/Application";
+
 function App() {
-  return <></>;
+  return (
+    <Layout>
+      <Application />
+    </Layout>
+  );
 }
 
 export default App;
