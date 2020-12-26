2d07ce01258ce6d9de106e5268dd6bd453464ad3
Areshka <oleg1990moskalenko@gmail.com>
2020-12-26 23:44:28 +0200

    added function for get weather data

---
+++ src/api/weather.service.js
@@ -0,0 +1,24 @@
+import axios from "./axios.instance";
+
+const APIkey = process.env.OPENWEATHERMAP_API_KEY;
+
+export const getWeatherDataFetch = async (city) => {
+  const { data } = await axios.get(
+    `/weather?q=${city}&units=metric&appid=${APIkey}`
+  );
+  return data;
+};
+
+export const getWeatherDataByIDFetch = async (id) => {
+  const { data } = await axios.get(
+    `/weather?id=${id}&units=metric&appid=${APIkey}`
+  );
+  return data;
+};
+
+export const getHourlyWeatherFetch = async (lat, lon) => {
+  const { data } = await axios.get(
+    `/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,daily&units=metric&appid=${APIkey}`
+  );
+  return data;
+};
