1d8b61082dd005305b994615b9c77dc17c7fd8f1
Areshka <oleg1990moskalenko@gmail.com>
2020-12-27 01:55:30 +0200

    added alertReducer to rootReducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,13 @@
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { combineReducers, createStore, applyMiddleware } from "redux";
+
+import { alertReducer } from "./alert/reducer";
 import citiesWeatherReducer from "./weather/reducer";
 
 const rootReducer = combineReducers({
   cities: citiesWeatherReducer,
+  alert: alertReducer
 });
 
 const store = createStore(
