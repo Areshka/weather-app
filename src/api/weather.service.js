8b1c509f581c5533122722c169fdab605250a44f
Areshka <oleg1990moskalenko@gmail.com>
2020-12-27 02:03:52 +0200

    added interseptors

--- src/api/weather.service.js
+++ src/api/weather.service.js
@@ -1,6 +1,30 @@
+import { showAlertThunk } from "../store/alert/thunks";
+import store from "../store/store";
 import axios from "./axios.instance";
 
 const APIkey = process.env.REACT_APP_API_KEY;
+
+axios.interceptors.request.use(
+  function (config) {
+    config.headers = {
+      ...config.headers,
+    };
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    store.dispatch(showAlertThunk(error.response.data.message));
+    return Promise.reject(error);
+  }
+);
 
 export const getWeatherDataFetch = async (city) => {
   const { data } = await axios.get(
