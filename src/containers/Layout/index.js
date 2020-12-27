c714e98892cd45c4869ec3e8da2c21586536881e
Areshka <oleg1990moskalenko@gmail.com>
2020-12-27 13:22:04 +0200

    added HashRouter for github pages

--- src/containers/Layout/index.js
+++ src/containers/Layout/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, HashRouter } from "react-router-dom";
 import store from "../../store/store";
 
 const Layout = ({ children }) => {
   return (
     <Provider store={store}>
-      <Router>{children}</Router>
+      <HashRouter>{children}</HashRouter>
     </Provider>
   );
 };
